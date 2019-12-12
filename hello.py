aa02cb606c024ec9f82c9ed8ca43afe36bb40407
JKS <juskus0310@naver.com>
2019-12-12 13:42:19 +0900

    for,if문 사용

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 from flask import Flask, escape, request, render_template
-
+import random
 app = Flask(__name__)
 #이코드는 flask 홈페이지에서 가저온 버전
 
@@ -48,10 +48,48 @@ def cube(num):
     cube_num = num**3
     return render_template('cube.html',num =num, cube_num = cube_num)
 
+@app.route('/lunch')
+def lunch():
+    # menu 리스트를 만들어주세요.
+    menu = ['치킨집', '양자강','감자탕']
+
+    #dic으로 작성하기
+    menus = {
+        "양자강" :"https://steemitimages.com/DQmNPAY4m1GVTVcmer7qmLkrUJ5G1eMeSPVefSbsUXKWQAD/20180403_114159.jpg",
+        "치킨집" : "https://www.mpps.co.kr/kfcs_api_img/KFCS/goods/DL_2172965_20181218164144426.png",
+        "감자탕" : "http://ebadom.com/upload/menu_01/2017_07_06/hero_uRpFU_2017_07_06_12_09_50.jpg"
+    }
+
+    menu_list = list(menus.keys())
+    pick = random.choice(menu_list)
+    img =menus[pick]
+    
+    
+
+
+    choicesrc= ""
+    choice = random.choice(menu)
+    if choice == '치킨집':
+        choicesrc= "https://www.mpps.co.kr/kfcs_api_img/KFCS/goods/DL_2172965_20181218164144426.png"
+    elif choice == '양자강':
+        choicesrc= "https://steemitimages.com/DQmNPAY4m1GVTVcmer7qmLkrUJ5G1eMeSPVefSbsUXKWQAD/20180403_114159.jpg"
+    elif choice =='감자탕':
+        choicesrc= "http://ebadom.com/upload/menu_01/2017_07_06/hero_uRpFU_2017_07_06_12_09_50.jpg"
+
+   
+
+    # return render_template('lunch.html',lunch_name=choice,lunch_image=choicesrc )
+    return render_template('lunch.html',lunch_name=pick,lunch_image=img )
+
+
+@app.route('/movies')
+def movies():
+    movies = ['겨울왕국2', '쥬만지', '포드v페라리']
+
+    return render_template('movies.html',movies =movies)
 
 
 
 
-#
 if __name__=='__main__':
     app.run(debug=True) #이걸로 인해 개발자 모드로 서버를 킬 필요가없음
