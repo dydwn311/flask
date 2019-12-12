84d427eb92f51721a9916df8f2bbee8e14c121f1
JKS <juskus0310@naver.com>
2019-12-12 10:30:17 +0900

    html로 응답하기

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, escape, request
+from flask import Flask, escape, request, render_template
 
 app = Flask(__name__)
 #이코드는 flask 홈페이지에서 가저온 버전
@@ -10,15 +10,28 @@ def hello():
     name = request.args.get("name", "World")
     return f'Hello, {escape(name)}!'
 
-if __name__=='__main__':
-    app.run(debug=True)
-
 #py란 파일이 실행되는 경우 이파일 실행되거나(run) import하는 경우     
 
 
 @app.route('/hi')
 def hi():
-    return 'hi'
+    return "hi"
+
+@app.route('/change')
+def change():
+    return 'hello change'
+
+
+
+@app.route('/html_tag')
+def html_tag():
+    return '<h1>안녕하세요</h1>'
+
+    
+@app.route('/html_file')
+def html_file():
+    return render_template('index.html')
+
 
 if __name__=='__main__':
     app.run(debug=True)
